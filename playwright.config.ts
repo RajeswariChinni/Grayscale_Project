f5ae0b7f34805467f23b1f79703dc0bac74c1606
Raja Rajeswari <rajeshaware.ch@gmail.com>
2025-09-24 15:33:59 +0530

    Added the final scenario3

--- playwright.config.ts
+++ playwright.config.ts
@@ -19,11 +19,6 @@ import { defineConfig, devices } from '@playwright/test';
 // >>>>>>> e8603e822d44719fa387a93d6433cf5d934ae943
 export default defineConfig({
   testDir: './tests',
-<<<<<<< HEAD
-  // other config options...
-
-=======
->>>>>>> origin/main
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
