077b07f2aa8b17ef75657df71bd997ede7f099f4
Raja Rajeswari <rajeshaware.ch@gmail.com>
2025-09-25 17:54:49 +0530

    added aftereach funtion in TS2

--- tests/TS2-BMICalculatorValidation.spec.ts
+++ tests/TS2-BMICalculatorValidation.spec.ts
@@ -2,10 +2,20 @@ import { test, Page} from '@playwright/test';
 import calcPage from './pages/calcPage';
 import * as dataset from './utilities/data.json';
 
-test.describe.serial('BMI Calculator Functionality', async () => {
+test.describe('BMI Calculator Functionality', async () => {
   test.beforeEach(async ({page})=>{
     await page.goto('https://vagdevimahendrada.github.io/BMICalculator/');
   })
+
+  test.afterEach(async ({ page }, testInfo) => {
+    // const sanitizedTitle = testInfo.title.replace(/\s+/g, '_').replace(/[^a-zA-Z0-9_]/g, '');
+    
+    await testInfo.attach(`Screenshot - ${testInfo.title}`, {
+      body: await page.screenshot(),
+      contentType: 'image/png',
+  });
+  await page.close()
+});
   
   for (const data of dataset.data){
     test(`Verify BMI calculation with ${data.height} ${data.heightMetric} and ${data.weight} ${data.weightMetric} @regression`,async({page})=>{
@@ -27,11 +37,6 @@ test.describe.serial('BMI Calculator Functionality', async () => {
     await logincredentials.weightselect('kg');
     await logincredentials.submitbutton();
     await page.waitForTimeout(3000);
-
-    test.info().attach('Error message on blank height field',{
-      body: await page.screenshot(),
-      contentType: 'image/png'
-    })
   })
 
   
@@ -42,11 +47,6 @@ test.describe.serial('BMI Calculator Functionality', async () => {
       await logincredentials.dropdown('cm');
       await logincredentials.submitbutton();
       await page.waitForTimeout(3000);
-
-      test.info().attach('Error message on blank weight field',{
-        body: await page.screenshot(),
-        contentType: 'image/png'
-      })
   })
   
   test('Verify error message when given height is negative @sanity @regression',async({page})=>{
@@ -58,11 +58,6 @@ test.describe.serial('BMI Calculator Functionality', async () => {
     await logincredentials.weightselect('kg');
     await logincredentials.submitbutton();
     await page.waitForTimeout(3000);
-
-    test.info().attach('Error message on blank height field',{
-      body: await page.screenshot(),
-      contentType: 'image/png'
-    })
   })
   
   test('Verify error message when given weight is negative @sanity @regression',async({page})=>{
@@ -74,11 +69,6 @@ test.describe.serial('BMI Calculator Functionality', async () => {
     await logincredentials.weightselect('kg');
     await logincredentials.submitbutton();
     await page.waitForTimeout(3000);
-
-    test.info().attach('Error message on blank weight field',{
-      body: await page.screenshot(),
-      contentType: 'image/png'
-    })
   })
 
 });
