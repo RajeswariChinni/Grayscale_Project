08401f5219890da22c61e819b658f5773ce9ee3e
zagabathunivaidehi <zagbathunivaidehi@gmail.com>
2025-09-30 09:04:12 +0530

    internal commit-2

--- tests/TS3-ResourcesPageValidation.spec.ts
+++ tests/TS3-ResourcesPageValidation.spec.ts
@@ -8,10 +8,8 @@ test.describe('Resource Page Functionality', async () => {
   })
 
   test.afterEach(async ({ page }, testInfo) => {  
-    await page.goBack();
-    await page.waitForLoadState();
-    await page.close();
     await page.waitForTimeout(2000);
+    await page.close();
   });
 
   test('Verify "WHO - Obsesity and Overweight Facts wellness" link @smoke @regression',async({page,context})=>{
@@ -24,6 +22,7 @@ test.describe('Resource Page Functionality', async () => {
     
     await page1.waitForLoadState();
     await expect.soft(page1).toHaveURL("https://www.who.int/news-room/fact-sheets/detail/obesity-and-overweight");
+    await page1.close();
   })
   
   test('Verify "CDC - BMI Information" link @smoke @regression',async({page,context})=>{
@@ -33,6 +32,7 @@ test.describe('Resource Page Functionality', async () => {
     ]);
     await page2.waitForLoadState();
     await expect.soft(page2).toHaveURL("https://www.cdc.gov/bmi/about/?CDC_AAref_Val=https://www.cdc.gov/healthyweight/assessing/bmi/index.html");
+    await page2.close();
   })
 
   test('Verify "What to do if you are underweight?" link @smoke @regression',async({page,context})=>{
@@ -43,6 +43,7 @@ test.describe('Resource Page Functionality', async () => {
     
     await page3.waitForLoadState();
     await expect.soft(page3).toHaveURL("https://www.healthdirect.gov.au/what-to-do-if-you-are-underweight");
+    await page3.close();
   })
 
 });
